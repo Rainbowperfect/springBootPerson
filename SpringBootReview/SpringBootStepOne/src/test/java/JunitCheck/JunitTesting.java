5ff3eb03373452ebcd1026bca17ff63ed37dc39b
RainbowPerfect <903933616@qq.com>
2019-04-11 22:47:46 +0800

    修改测试类

--- SpringBootReview/SpringBootStepOne/src/test/java/JunitCheck/JunitTesting.java
+++ SpringBootReview/SpringBootStepOne/src/test/java/JunitCheck/JunitTesting.java
@@ -5,15 +5,30 @@ import org.junit.runner.RunWith;
 import org.person.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = JunitTesting.class)
+
 public class JunitTesting{
-
    @Autowired
-   private  User user;
+   private ApplicationContext context;
 
+    /**
+     * 测试创建的组件config能否有效
+     */
+
+    @Test
+   public void testBean(){
+       boolean b = context.containsBean("HelloService");
+       System.out.println(b);
+   }
+
+
+
+    @Autowired
+    private User user;
     @Test
     public void testUser(){
         Integer age = user.getAge();
