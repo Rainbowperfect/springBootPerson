85f5e09e804d5a4e049d6307316e21ebc77936ee
RainbowPerfect <903933616@qq.com>
2019-04-17 12:22:20 +0800

    添加了config

---
+++ SpringBootFirstWeb/src/main/java/org/person/config/MyConfig.java
@@ -0,0 +1,24 @@
+package org.person.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+@Configuration
+//@EnableWebMvc
+public class MyConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public WebMvcConfigurerAdapter webMvcConfigurerAdapter(){
+        WebMvcConfigurerAdapter adapter = new WebMvcConfigurerAdapter() {
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addViewController("/").setViewName("index");
+                registry.addViewController("/index.html").setViewName("index");
+            }
+        };
+        return adapter;
+    }
+}
