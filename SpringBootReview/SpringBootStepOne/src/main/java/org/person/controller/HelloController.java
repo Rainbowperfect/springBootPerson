0f3a2cd3209c0f3db0a81539b79cdd6ad9574e77
RainbowPerfect <903933616@qq.com>
2019-04-24 11:07:02 +0800

    添加对Druid的管理

--- SpringBootReview/SpringBootStepOne/src/main/java/org/person/controller/HelloController.java
+++ SpringBootReview/SpringBootStepOne/src/main/java/org/person/controller/HelloController.java
@@ -2,20 +2,30 @@ package org.person.controller;
 
 import org.person.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+import java.util.Map;
+
+@RestController
 public class HelloController {
 
     @Autowired
     private User user;
 
-    @ResponseBody
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
     @RequestMapping("Hello")
     public User sayHi(){
 
         return  user;
+    }
+
+    @RequestMapping("/query")
+    public Map<String,Object> map(){
+        List<Map<String, Object>> list = jdbcTemplate.queryForList("select * from department");
+        return list.get(0);
     }
 }
