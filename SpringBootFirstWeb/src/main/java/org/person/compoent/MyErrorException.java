83fe3d1514d5a37fbb632ee073ea180292dbd821
RainbowPerfect <903933616@qq.com>
2019-04-25 11:47:28 +0800

    提交进度代码

---
+++ SpringBootFirstWeb/src/main/java/org/person/compoent/MyErrorException.java
@@ -0,0 +1,9 @@
+package org.person.compoent;
+
+import org.springframework.boot.autoconfigure.web.DefaultErrorAttributes;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyErrorException extends DefaultErrorAttributes {
+
+}
